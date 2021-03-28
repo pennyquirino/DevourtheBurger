8ff6753fb2dd5097d5816df3ae03f02dc759aaec
Money Penny <pennyquirino@gmail.com>
2021-03-28 12:16:59 -0400

    redid server code

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 3030;
 const app = express();
+const exphbs = require("express-handlebars");
+const path = require("path");
 
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exHbs = require("express-handlebars");
-
-app.engine("handlebars", exHbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server listening on: ${PORT}`);
+  }
 });
